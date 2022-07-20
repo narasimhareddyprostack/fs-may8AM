d0f2265ed93d08938eb3c7e0071a8995c53e73a7
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-07-20 12:09:19 +0530

    okie

--- react-5/src/App.js
+++ react-5/src/App.js
@@ -1,9 +1,10 @@
 import Show from './CondRendering/Show'
+import User from './AxiosEx/User'
 let App = () => {
 
     return <>
         <h1>App Component</h1>
-        <Show />
+        <User />
     </>
 }
 export default App
---
+++ react-5/src/AxiosEx/User.js
@@ -0,0 +1,31 @@
+import Axios from 'axios'
+import React from 'react'
+
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        }
+
+    }
+    get_Users = () => {
+        console.log("test Case 123")
+        Axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                console.log(response.data)
+                this.setState({ users: response.data })
+            })
+            .catch(() => { })
+    }
+    render() {
+        return <>
+            <pre>{JSON.stringify(this.state)} </pre>
+            <h3>User Data</h3>
+            <button onClick={this.get_Users}>Get Data</button>
+        </>
+    }
+}
+
+
+export default User
